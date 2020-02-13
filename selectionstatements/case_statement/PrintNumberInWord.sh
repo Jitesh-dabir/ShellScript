e4bbe9e2e474dc7b4a158b14340b6a88848ab4ce
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-13 14:39:12 +0530

    MODIFIED:Space added

--- selectionstatements/case_statement/PrintNumberInWord.sh
+++ selectionstatements/case_statement/PrintNumberInWord.sh
@@ -1,29 +1,29 @@
-#! /bin/bash -x
+#!/bin/bash -x
 
 #TAKE NUMBER FROM USER
-read -p "Enter the number:"number
+read -p "Enter the number:" number
 
 #PRINT NUMBER IN WORD
 case $number in
-1 )
-echo "Unit"
-;;
-10 )
-echo "Ten"
-;;
-100 )
-echo "Hundred"
-;;
-1000 )
-echo "One Thousand"
-;;
-10000 )
-echo "Ten Thousand"
-;;
-100000 )
-echo "Hundred Thousand"
-;;
-1000000 )
-echo "One Billoins"
-;;
+	1)
+		echo "Unit"
+		;;
+	10)
+		echo "Ten"
+		;;
+	100)
+		echo "Hundred"
+		;;
+	1000)
+		echo "One Thousand"
+		;;
+	10000)
+		echo "Ten Thousand"
+		;;
+	100000)
+		echo "Hundred Thousand"
+		;;
+	1000000)
+		echo "One Billoins"
+		;;
 esac
