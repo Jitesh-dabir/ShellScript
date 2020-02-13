1ee4cfeaf86676da5c0f98a43a1e8772da0ea3d7
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-13 09:12:43 +0530

    Added:Program to perform addition of two numbers

---
+++ programmingconcepts/Additionoftwonumbers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+#TAKE TWO NUMBERS FROM USER 
+read -p "Enter First_Number :" firstNumber
+read -p "Enter Second_Number :" secondNumber
+
+#ADDITION OF TWO NUMBERS
+result=$(($firstNumber + $secondNumber))
+
+#PRINT TWO  NUMBERS
+echo "Addition of two numbers are " $result
+
