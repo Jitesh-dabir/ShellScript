31b4ba37322a18b09f5fe72c71f4567543129a1a
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-13 16:02:27 +0530

    MODIFIED:Index added (loop)

--- programmingconstructs/forloop/PrintHarmonicNumber.sh
+++ programmingconstructs/forloop/PrintHarmonicNumber.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
 #VARIABLE
-res=1
+result=1
 
 #TAKE NUMBER FROM USER
 read -p  "Enter a number :" number
 
 #PRINT HARMONIC NUMBER
-for (( i=1; i<=$number; i++))
+for (( index=1; index<=$number; index++))
 do
-	num=1/$i
-	res=`echo "scale=2;$res+$num" | bc `
-	echo -n "1/$i +"
+	firstNumber=1/$index
+	result=`echo "scale=2;$result+$firstNumber" | bc `
+	echo -n "1/$index +"
 done
 
 #PRINT ADDITION OF N'TH HARMONIC NUMBER
-echo "Addition upto" 1/$number" harmonic number is" $res
+echo "Addition upto" 1/$number" harmonic number is" $result
