7a9c120463e40d23a522f0c5e40bd2b777fc74b5
Jitesh-Dabir <jitudabir9@gmail.com>
2020-02-14 11:22:33 +0530

    ADDED:Program to find birtdays in same month using dictionary

---
+++ programmingconstructs/dictionary/BirthDaysInSameMonth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# VARIABLE INITIALIZATION
+limit=50
+
+# DECLARE DICTIONARY
+declare -A month
+
+# CALCULATE 50 RANDOM MONTHS
+for (( i=1; i<=$limit; i++ ))
+do
+	random=$(( 1 + RANDOM % 12 ))
+	month[$random]=$(( ${month[$random]} + 1 ))
+done
+
+# DISPLAY INDIVIDUALS HAVING BIRTHDAYS IN THE SAME MONTH
+echo "Individuals having birthdays in same month are:"
+for (( i=1; i<=12; i++ ))
+do
+	echo $i=${month[$i]}
+done
